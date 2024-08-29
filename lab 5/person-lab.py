14cab372ffc4fd15eb80907bb12b95d7f2fe8899
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-08-29 22:02:41 +0300

    add lab 5 answers

---
+++ lab 5/person-lab.py
@@ -0,0 +1,13 @@
+"""
+    class Person:
+    Private Attributes:
+        __name:
+        __brith year:
+
+    methods:
+        say_hello: prints name whenever called
+
+    properties:
+        name
+        age
+"""
